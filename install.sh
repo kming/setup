be82ffc5fee803e613268b4062f702af5193e6d3
Kei Ming Kwong <kmingk.work@gmail.com>
2023-05-29 22:02:47 -0400

    Add install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# install oh-my-zsh
+ZSH=~/.setup/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install fzf
+./third-party/fzf/install
