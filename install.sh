8f2a740d4195488c5b59b5d1070e7faedd2edc91
Kei Ming Kwong <kmingk.work@gmail.com>
2023-05-29 22:37:08 -0400

    Updated installation scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # install oh-my-zsh
-ZSH=~/.setup/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no ZSH=~/.setup/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install fzf
 ./third-party/fzf/install
+
+# link powerlevel10k
+ln -s ~/.setup/third-party/powerlevel10k/ .oh-my-zsh/custom/themes/powerlevel10k
+
+# copy zshrc
+cp .zshrc ~/.zshrc
--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,10 @@
 
 # install oh-my-zsh
 ZSH=~/.setup/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+rm -rf .oh-my-zsh
 
 # install fzf
 ./third-party/fzf/uninstall
+
+rm ~/.zshrc
+
