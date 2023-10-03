9e98a719bb52b5fa545c57cfce024832d8433afa
kkwong10 <kkwong@tenstorrent.com>
2023-10-03 19:37:59 +0000

    [infra] Update build scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+`git submodule update --recursive --init`
+
+if [ ! -n  `which zsh` ]; then
+   # 
+   echo "Need to install ZSH for this setup"
+   exit 1 
+fi
+
 git_root=`git rev-parse --show-toplevel`
 omz_root="${git_root}/.oh-my-zsh" 
 
@@ -7,10 +15,10 @@ omz_root="${git_root}/.oh-my-zsh"
 RUNZSH=no ZSH="${omz_root}" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install fzf
-./third-party/fzf/install
+./third-party/fzf/install --all
 
 # link powerlevel10k
-ln -s "${git_root}/third-party/powerlevel10k/" "${omz_root}/custom/themes/powerlevel10k"
+ln -sf "${git_root}/third-party/powerlevel10k/" "${omz_root}/custom/themes/powerlevel10k"
 
 # copy dotfiles
 cp .zshrc ~/.zshrc
